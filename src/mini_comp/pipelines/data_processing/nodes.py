eab2441433af75bd221d491a3b5be1cea4e8fbf5
SSim80 <158095215+SSim80@users.noreply.github.com>
2024-07-18 17:13:11 +0200

    Rounding and docstrings
    Added rounding to the nearst 2 decimals to the preprocess_cities function in processing nodes.py and added some docstrings

--- src/mini_comp/pipelines/data_processing/nodes.py
+++ src/mini_comp/pipelines/data_processing/nodes.py
@@ -117,6 +117,21 @@ import pandas as pd
 
 
 def split_cities(initial_data_train_labels: pd.DataFrame, initial_data_train_features: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    """
+    Split the initial data into separate dataframes for San Juan and Iquitos.
+
+    Args:
+        initial_data_train_labels (pd.DataFrame): The initial data train labels dataframe.
+        initial_data_train_features (pd.DataFrame): The initial data train features dataframe.
+
+    Returns:
+        Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]: A tuple containing the following dataframes:
+            - sj_train_features: The train features dataframe for San Juan.
+            - sj_train_labels: The train labels dataframe for San Juan.
+            - iq_train_features: The train features dataframe for Iquitos.
+            - iq_train_labels: The train labels dataframe for Iquitos.
+    """
+    
     # Seperate data for San Juan
     sj_train_features = initial_data_train_features.loc['sj']
     sj_train_labels = initial_data_train_labels.loc['sj']
@@ -129,9 +144,26 @@ def split_cities(initial_data_train_labels: pd.DataFrame, initial_data_train_fea
 
 
 def preprocess_cities(sj_train_features: pd.DataFrame, iq_train_features: pd.DataFrame, sj_train_labels: pd.DataFrame, iq_train_labels: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    """
+    Preprocesses the city train features and labels.
+
+    Args:
+        sj_train_features (pd.DataFrame): The San Juan train features.
+        iq_train_features (pd.DataFrame): The Iquitos train features.
+        sj_train_labels (pd.DataFrame): The San Juan train labels.
+        iq_train_labels (pd.DataFrame): The Iquitos train labels.
+
+    Returns:
+        Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]: A tuple containing the preprocessed San Juan train data and labels, and the preprocessed Iquitos train data and labels.
+    """
+    
     # Fill missing values with the mean of the column
     sj_train_features_imputed = sj_train_features.fillna(method='ffill')
     iq_train_features_imputed = iq_train_features.fillna(method='ffill')
+
+    # Round all float or double numbers to 2 decimal places
+    sj_train_features_imputed = sj_train_features.round(2)
+    iq_train_features_imputed = iq_train_features.round(2)
 
     # select features we want
     features = ['reanalysis_specific_humidity_g_per_kg',
