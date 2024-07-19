52464ebc413177f8a0f955683c1acf7223a26cd1
SSim80 <158095215+SSim80@users.noreply.github.com>
2024-07-19 10:54:53 +0200

    data science pipeline setup

--- src/mini_comp/pipelines/data_science/pipeline.py
+++ src/mini_comp/pipelines/data_science/pipeline.py
@@ -1,28 +1,35 @@
 from kedro.pipeline import Pipeline, node, pipeline
 
-from .nodes import evaluate_model, split_data, train_model
+from .nodes import write_output_file, wrapper_split_datasplit_data, wrapper_get_best_model, wrapper_plot_fitted_values
 
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline(
         [
-            # node(
-            #     func=split_data,
-            #     inputs=["model_input_table@pandas", "params:model_options"],
-            #     outputs=["X_train", "X_test", "y_train", "y_test"],
-            #     name="split_data_node",
-            # ),
-            # node(
-            #     func=train_model,
-            #     inputs=["X_train", "y_train"],
-            #     outputs="regressor",
-            #     name="train_model_node",
-            # ),
-            # node(
-            #     func=evaluate_model,
-            #     inputs=["regressor", "X_test", "y_test"],
-            #     outputs="metrics",
-            #     name="evaluate_model_node",
-            # ),
+            node(
+                func=wrapper_split_datasplit_data,
+                inputs=["sj_data", "iq_data"],
+                outputs=["sj_train", "sj_test", "iq_train", "iq_test"],
+                name="split_data_node",
+            ),
+            node(
+                func=wrapper_get_best_model,
+                inputs=["sj_train", "sj_test", "iq_train", "iq_test"],
+                outputs=["sj_fitted_model", "iq_fitted_model"],
+                name="get_best_model_node",
+            ),
+            node(
+                func=wrapper_plot_fitted_values,
+                inputs=["sj_train", "sj_fitted_model", "iq_train", "iq_fitted_model"],
+                outputs=['sj_figs', 'iq_figs'],
+                name="plot_fitted_values_node",
+            ),
+            node(
+                func=write_output_file,
+                inputs=["sj_test", "iq_test", "sj_best_model", "iq_best_model"],
+                outputs="submission",
+                name="write_output_file_node",
+            ),
+            
         ]
     )
